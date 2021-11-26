9bd4025248f6f52c99a354eaad55807701ebdfbe
Aakash <ajangid25@gmail.com>
2021-11-26 21:05:21 +0530

    Updated server_start script to trigger correct .jar file

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -3,4 +3,4 @@
 cd /home/ec2-user/server
 
 sudo $(which java) -jar -Dserver.port=80 \
-     todoapp-*.jar > /dev/null 2> /dev/null < /dev/null &
+      spring-boot-todo.jar > /dev/null 2> /dev/null < /dev/null &
